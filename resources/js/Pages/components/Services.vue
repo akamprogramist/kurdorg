e0bec5ae68842a7210b29caf5b84c065350aa2af
Akam Programist <akadev404@gmail.com>
2023-03-23 21:37:30 +0300

    commits

--- resources/js/Pages/components/Services.vue
+++ resources/js/Pages/components/Services.vue
@@ -1,20 +1,68 @@
 <template>
-    <Container class="bg-gray-200">
-        <div class="py-5">
-            <a
-                v-for="(service, index) in Services"
-                :key="index"
-                href="/localNGOs"
-            >
-                <div class="flex px-5 py-6 bg-white space-x-5 rounded-lg my-16">
+    <Container class="bg-gray-100">
+        <div class="text-center space-y-1 pt-10">
+            <p class="font-medium uppercase">Lorem ipsa amet.</p>
+            <p class="text-3xl font-bold capitalize">
+                find Opportunities around the world
+            </p>
+        </div>
+        <div class="grid gap-10 py-10 md:flex">
+            <a href="/localNGOs" class="group duration-300">
+                <div
+                    class="flex px-5 py-6 bg-white group-hover:bg-redsh space-x-5 rounded-lg duration-300"
+                >
                     <i
-                        class="fa-solid mt-1 text-5xl text-redsh"
-                        :class="service.icon"
+                        class="fa-solid fa-building-ngo mt-1 text-redsh text-5xl group-hover:text-white"
                     ></i>
                     <div class="block space-y-2">
-                        <p class="text-lg font-bold">{{ service.title }}</p>
-                        <p class="font-medium text-gray-500">
-                            {{ service.description }}
+                        <p class="text-lg font-bold group-hover:text-white">
+                            Local NGOs
+                        </p>
+                        <p
+                            class="font-medium text-gray-500 group-hover:text-white"
+                        >
+                            Lorem, ipsum dolor sit amet consectetur
+                            adipisicingelit
+                        </p>
+                    </div>
+                </div>
+            </a>
+            <a href="/localNGOs" class="group duration-300">
+                <div
+                    class="flex px-5 py-6 bg-white group-hover:bg-yellowsh space-x-5 rounded-lg duration-300"
+                >
+                    <i
+                        class="fa-solid fa-building-un mt-1 text-yellowsh text-5xl group-hover:text-white"
+                    ></i>
+                    <div class="block space-y-2">
+                        <p class="text-lg font-bold group-hover:text-white">
+                            International NGOs
+                        </p>
+                        <p
+                            class="font-medium text-gray-500 group-hover:text-white"
+                        >
+                            Lorem, ipsum dolor sit amet consectetur
+                            adipisicingelit
+                        </p>
+                    </div>
+                </div>
+            </a>
+            <a href="/localNGOs" class="group duration-300">
+                <div
+                    class="flex px-5 py-6 bg-white group-hover:bg-bluesh space-x-5 rounded-lg duration-300"
+                >
+                    <i
+                        class="fa-solid fa-briefcase mt-1 text-bluesh text-5xl group-hover:text-white"
+                    ></i>
+                    <div class="block space-y-2">
+                        <p class="text-lg font-bold group-hover:text-white">
+                            Opportunities
+                        </p>
+                        <p
+                            class="font-medium text-gray-500 group-hover:text-white"
+                        >
+                            Lorem, ipsum dolor sit amet consectetur
+                            adipisicingelit
                         </p>
                     </div>
                 </div>
@@ -28,19 +76,20 @@ import Container from "../layout/Container.vue";
 
 let Services = [
     {
-        title: "Local NGOs",
-        description: "Lorem, ipsum dolor sit amet consectetur adipisicingelit",
-        icon: "fa-building-ngo",
+        title: "",
+        description: "",
     },
     {
         title: "International NGOs",
         description: "Lorem, ipsum dolor sit amet consectetur adipisicingelit",
         icon: "fa-building-un",
+        color: "yellowsh",
     },
     {
         title: "Opportunities",
         description: "Lorem, ipsum dolor sit amet consectetur adipisicingelit",
         icon: "fa-briefcase",
+        color: "bluesh",
     },
 ];
 </script>
